a9660672e49c29e589b0e3ad1d698b70a53a3a6b
Gerardociscoman <57124182+Gerardociscoman@users.noreply.github.com>
2025-12-10 12:40:07 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,30 @@
+from flask import Flask, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="db",
+        user="root",
+        password="password",
+        database="testdb"
+    )
+
+@app.route("/")
+def home():
+    return "Flask + Nginx + MySQL running!"
+
+@app.route("/users")
+def users():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(rows)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
 
